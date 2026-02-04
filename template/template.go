cd175e15b6ba6a9ecc3d91800b42a034b61147c3
HIM049 <HIM_049@163.com>
2026-02-05 02:02:54 +0800

    refactor: tmpl name

--- template/template.go
+++ template/template.go
@@ -7,5 +7,5 @@ var Tmpl *template.Template
 // Init initializes the template.
 // It panics when some error occurs.
 func Init() {
-	Tmpl = template.Must(template.ParseGlob("templates/*.gtpl"))
+	Tmpl = template.Must(template.ParseGlob("templates/*.html"))
 }

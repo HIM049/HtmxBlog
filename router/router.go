59d80dc6414a7a4b3e1c92ab2a3d570271f3a393
HIM049 <HIM_049@163.com>
2026-02-05 13:03:58 +0800

    feat: tailwind & air

--- router/router.go
+++ router/router.go
@@ -13,6 +13,8 @@ func Init() *chi.Mux {
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 
+	r.Handle("/assets/*", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		template.Tmpl.ExecuteTemplate(w, "index", template.App{PageTitle: "Hello World", Navigation: []template.NavigationItem{{Name: "Home", Url: "/"}, {Name: "About", Url: "/about"}}})

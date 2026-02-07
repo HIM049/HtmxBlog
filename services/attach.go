ed8f15f93a0bc799a2d329635a620f5dd726bb0f
HIM049 <HIM_049@163.com>
2026-02-08 01:46:54 +0800

    fix: duplicate file clean

--- services/attach.go
+++ services/attach.go
@@ -18,16 +18,23 @@ import (
 const ATTACHES_DIR = "./app_data/attaches"
 
 func UploadAttach(file *multipart.File, name, mime string) (*model.Attach, error) {
+	isSuccess := false
+
 	// Generate unique ID (UID)
 	uuid := uuid.New().String()
-
 	dstPath := filepath.Join(ATTACHES_DIR, uuid)
+
 	// create local file
 	dst, err := os.Create(dstPath)
 	if err != nil {
 		return nil, err
 	}
-	defer dst.Close()
+	defer func() {
+		dst.Close()
+		if !isSuccess {
+			os.Remove(dstPath)
+		}
+	}()
 
 	// create hasher
 	hasher := sha256.New()
@@ -40,11 +47,11 @@ func UploadAttach(file *multipart.File, name, mime string) (*model.Attach, error
 	// check if attach already exists
 	attach, err := database.ReadAttachByHash(hash)
 	if err == nil {
-		os.Remove(dstPath)
 		return attach, nil
 	} else {
 		if !errors.Is(err, gorm.ErrRecordNotFound) {
 			// if error happend and is db err
+			dst.Close()
 			os.Remove(dstPath)
 			return nil, err
 		}
@@ -60,8 +67,10 @@ func UploadAttach(file *multipart.File, name, mime string) (*model.Attach, error
 	// record to db
 	err = database.CreateAttach(attach)
 	if err != nil {
-		os.Remove(dstPath)
+		return nil, err
 	}
+
+	isSuccess = true
 
 	return attach, nil
 

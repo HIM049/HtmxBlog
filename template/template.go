c980b331c1283a32b8ae7f07970e376f390ce96e
HIM049 <HIM_049@163.com>
2026-02-08 01:04:32 +0800

    refactor: set dict

--- template/template.go
+++ template/template.go
@@ -1,11 +1,32 @@
 package template
 
-import "html/template"
+import (
+	"html/template"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
 var Tmpl *template.Template
 
 // Init initializes the template.
 // It panics when some error occurs.
 func Init() {
-	Tmpl = template.Must(template.ParseGlob("templates/*.html"))
+	var files []string
+
+	err := filepath.Walk("templates", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() && strings.HasSuffix(path, ".html") {
+			files = append(files, path)
+		}
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	Tmpl = template.Must(template.ParseFiles(files...))
 }

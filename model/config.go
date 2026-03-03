80a32fd5704b4b2a6356fd8413fdab88d9fdc345
HIM049 <HIM_049@163.com>
2026-03-03 23:01:49 +0800

    fix: index length on mysql

--- model/config.go
+++ model/config.go
@@ -4,6 +4,6 @@ import "gorm.io/gorm"
 
 type Config struct {
 	gorm.Model
-	Key   string `json:"key" gorm:"uniqueIndex"`
+	Key   string `json:"key" gorm:"uniqueIndex;not null;size:255"`
 	Value string `json:"value"`
 }
